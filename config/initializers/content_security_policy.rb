c6b0d0a30ca4842b560ccd8e79caea009eca4976
Josseph Downs <downsjosseph@gmail.com>
2025-09-05 17:31:44 +0000

    allowing api fetching

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,30 +1,30 @@
 # Be sure to restart your server when you modify this file.
 
-# Define an application-wide content security policy
-# For further information see the following documentation
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+Rails.application.config.content_security_policy do |policy|
+  # Core
+  policy.default_src :self
+  policy.object_src  :none
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
-#   # If you are using webpack-dev-server then specify webpack-dev-server host
-#   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+  # Assets
+  policy.img_src   :self, :https, :data
+  policy.font_src  :self, :https, :data
+  policy.style_src :self, :https, :unsafe_inline
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+  # Scripts (allow inline since you use inline <script>)
+  policy.script_src :self, :https, :unsafe_inline
 
-# If you are using UJS then enable automatic nonce generation
+  # XHR/fetch targets (add any others you need)
+  policy.connect_src :self, :https, "https://api.countapi.xyz"
+
+  # Dev server sockets in development only
+  if Rails.env.development?
+    policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035"
+  end
+
+  # Optional: CSP report endpoint
+  # policy.report_uri "/csp-violation-report-endpoint"
+end
+
+# If you prefer nonces instead of :unsafe_inline, enable:
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
-
-# Set the nonce only to specific directives
 # Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
-
-# Report CSP violations to a specified URI
-# For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
