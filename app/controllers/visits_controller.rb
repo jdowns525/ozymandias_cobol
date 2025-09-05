287b6966d4e13d30707a7e47ef922f146f77a31e
Josseph Downs <downsjosseph@gmail.com>
2025-09-05 17:46:02 +0000

    fixing typos

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,10 +1,11 @@
+# app/controllers/visits_controller.rb
 class VisitsController < ApplicationController
   protect_from_forgery with: :null_session
 
   def hit
-    key = "visits:all"                    # single, global counter
+    key   = "visits:all"
     value = (Rails.cache.read(key).to_i) + 1
-    Rails.cache.write(key, value)         # no expiry; persists in cache store
+    Rails.cache.write(key, value)
     render json: { value: value }
   end
 end
